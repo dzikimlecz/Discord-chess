878a3846751e0716d4c55c31d018a9414747d08e
dzikimlecz <j.pul312@gmail.com>
2020-12-20 17:23:27 +0100

    Introduced one method for getting the pieces from board, added method to get king, specified square and squares between another 2. Added some documentation

--- src/main/java/me/dzikimlecz/discordchess/chess/game/board/Board.java
+++ src/main/java/me/dzikimlecz/discordchess/chess/game/board/Board.java
@@ -1,13 +1,15 @@
-package me.dzikimlecz.discordchess.chess.board;
+package me.dzikimlecz.discordchess.chess.game.board;
 
-import me.dzikimlecz.discordchess.chess.Color;
-import me.dzikimlecz.discordchess.chess.pieces.*;
+import me.dzikimlecz.discordchess.chess.game.board.pieces.*;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static me.dzikimlecz.discordchess.chess.Color.*;
+import static me.dzikimlecz.discordchess.chess.game.board.Color.BLACK;
+import static me.dzikimlecz.discordchess.chess.game.board.Color.WHITE;
 
 public class Board {
 	private final Square[][] theBoard;
@@ -43,36 +45,97 @@ public class Board {
 		theBoard[7][4].putPiece(new King(BLACK, theBoard[7][4]));
 	}
 
-	//may be changed i used it for debugging
-	@Override
-	public String toString() {
-		return Arrays.deepToString(theBoard);
-	}
-
 	/**
-	 * parses from chess notation to coordinates in the array of squares (a, 1 -> 0, 0)
-	 * @param row
-	 * @param line
-	 * @return
+	 * parses from chess notation to coordinates in the array of squares e.g. (a, 1 -> 0, 0)
+	 * @param row row of the board (1-8)
+	 * @param line line of the board (a-h)
+	 * @return array mapping both row and line to ints in closed range from 0 to 7
 	 */
 	private int[] parseCoords(int row, char line) {
 		return new int[]{row-1, line - 'a'};
 	}
 
+	/**
+	 * gets square on specified chess notation
+	 * @param line line of the board (a-h)
+	 * @param row row of the board (1-8)
+	 * @return square on the specified location
+	 */
+	public Square square(char line, int row) {
+		int[] coords = parseCoords(row, line);
+		return theBoard[coords[0]][coords[1]];
+	}
+
+
+	/**
+	 * Gets list of all squares lying in space between other 2 (exclusive of them).<br>
+	 * MAY PRODUCE BUGS WHEN SQUARES ARE NOT ON THE SAME LINE OR DIAGONAL - IT'S MISUSE
+	 * @param square start of the space to be returned.
+	 * @param square1 end of the space to be returned.
+	 * @return all squares between {@code square} and {@code square1}
+	 */
+	public List<Square> squaresBetween(Square square, Square square1) {
+		return squaresBetween(square, square1, false);
+	}
+
+	/**
+	 * Gets list of all squares lying in space between other 2.<br>
+	 * MAY PRODUCE BUGS WHEN SQUARES ARE NOT ON THE SAME LINE OR DIAGONAL - IT'S MISUSE
+	 * @param square start of the space to be returned.
+	 * @param square1 end of the space to be returned.
+	 * @param inclusive are the squares on the edges supposed to be included.
+	 * @return all squares between {@code square} and {@code square1}
+	 */
+	public List<Square> squaresBetween(Square square, Square square1, boolean inclusive) {
+		final int lesserChange = inclusive ? 0 : 1;
+		final int biggerChange = inclusive ? 1 : 0;
+		final int lesserY = Math.min(square.getRow(), square1.getRow()) + lesserChange;
+		final int biggerY = Math.max(square.getRow(), square1.getRow()) + biggerChange;
+		final char lesserX = (char) (Math.min(square.getLine(), square1.getLine()) + lesserChange);
+		final char biggerX = (char) (Math.max(square.getLine(), square1.getLine()) + biggerChange);
+
+		List<Square> squares = new ArrayList<>();
+		for (int y = lesserY; y < biggerY; y++)
+			for (char x = lesserX; x < biggerX; x++)
+				squares.add(this.square(x, y));
+
+		return List.copyOf(squares);
+	}
+
 	public List<? extends Piece> getPiecesMovingTo(char line,
 	                                               int row,
-	                                               Class<? extends Piece> type,
-	                                               Color color) {
+	                                               @Nullable Class<? extends Piece> type,
+	                                               @NotNull Color color) {
+
 		if (type == Pawn.class) return getPawnsNearby(line, row, color);
 		if (type == Knight.class) return getKnightsAttacking(line, row, color);
 		if (type == Bishop.class) return getBishopsOnDiagonals(line, row, color);
 		if (type == Rook.class) return getRooksOnLines(line, row, color);
 		if (type == Queen.class) return getQueenFromLineOrDiagonal(line, row, color);
-		return getKingNearby(line ,row);
+		if (type == King.class) return getKingNearby(line, row, color);
+
+		List<Class<? extends Piece>> distantAttackingTypes = List.of(
+				Knight.class,
+				Bishop.class,
+				Rook.class,
+				Queen.class
+		);
+
+		if (type == null) {
+			List<Piece> pieces = new ArrayList<>();
+			distantAttackingTypes.forEach(
+					clazz -> pieces.addAll(getPiecesMovingTo(line, row, clazz, color)));
+			return List.copyOf(pieces);
+		}
+
+		List<Piece> pieces = new ArrayList<>(getPiecesMovingTo(line, row, null, color));
+		pieces.addAll(getPiecesMovingTo(line, row, Pawn.class, color));
+		pieces.addAll(getPiecesMovingTo(line, row, King.class, color));
+		return List.copyOf(pieces);
 	}
 
 
-	public List<Pawn> getPawnsNearby(char line, int row, Color color) {
+	private List<Pawn> getPawnsNearby(char line, int row, Color color) {
 		List<Pawn> pawns = new ArrayList<>();
 		int[] coords = parseCoords(row, line);
 		final int startingY = coords[0];
@@ -96,18 +159,7 @@ public class Board {
 		return List.copyOf(pawns);
 	}
 
-	/**
-	 * gets square on specified chess notation
-	 * @param line
-	 * @param row
-	 * @return
-	 */
-	public Square square(char line, int row) {
-		int[] coords = parseCoords(row, line);
-		return theBoard[coords[0]][coords[1]];
-	}
-
-	public List<Bishop> getBishopsOnDiagonals(char line, int row, Color color) {
+	private List<Bishop> getBishopsOnDiagonals(char line, int row, Color color) {
 		List<Bishop> bishops = new ArrayList<>();
 		int[] coords = parseCoords(row, line);
 		final int startingY = coords[0];
@@ -131,7 +183,7 @@ public class Board {
 		return List.copyOf(bishops);
 	}
 
-	public List<King> getKingNearby(char line, int row) {
+	private List<King> getKingNearby(char line, int row, Color color) {
 		var center = parseCoords(row, line);
 		final int centerY = center[0];
 		final int centerX = center[1];
@@ -140,13 +192,14 @@ public class Board {
 			for (int xDelta = -1; xDelta <= 1; xDelta++) {
 				int x = centerX + xDelta;
 				Piece piece = theBoard[y][x].getPiece();
-				if (piece instanceof King) return List.of((King) piece);
+				if (piece instanceof King && piece.getColor() == color)
+					return List.of((King) piece);
 			}
 		}
 		return List.of();
 	}
 
-	public List<Knight> getKnightsAttacking(char line, int row, Color color) {
+	private List<Knight> getKnightsAttacking(char line, int row, Color color) {
 		List<Knight> knights = new ArrayList<>();
 		int[] coords = parseCoords(row, line);
 		final int y = coords[0];
@@ -168,7 +221,7 @@ public class Board {
 		return List.copyOf(knights);
 	}
 
-	public List<Queen> getQueenFromLineOrDiagonal(char line, int row, Color color) {
+	private List<Queen> getQueenFromLineOrDiagonal(char line, int row, Color color) {
 		List<Queen> queens = new ArrayList<>();
 		int[] coords = parseCoords(row, line);
 		final int startingY = coords[0];
@@ -203,7 +256,7 @@ public class Board {
 
 	}
 
-	public List<Rook> getRooksOnLines(char line, int row, Color color) {
+	private List<Rook> getRooksOnLines(char line, int row, Color color) {
 		List<Rook> rooks = new ArrayList<>();
 		int[] coords = parseCoords(row, line);
 		final int y = coords[0];
@@ -219,5 +272,21 @@ public class Board {
 				rooks.add((Rook) piece);
 		}
 		return List.copyOf(rooks);
+	}
+
+	public King getKing(Color color) {
+		for (Square[] squares : theBoard) {
+			for (Square square : squares) {
+				Piece piece = square.getPiece();
+				if (piece instanceof King && piece.getColor() == color) return (King) piece;
+			}
+		}
+		throw new RuntimeException();
+	}
+
+	//may be changed i used it for debugging
+	@Override
+	public String toString() {
+		return Arrays.deepToString(theBoard);
 	}
 }
