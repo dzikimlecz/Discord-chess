130bd48f32c72726115ea90620247216d8f579ea
dzikimlecz <j.pul312@gmail.com>
2021-01-09 16:53:31 +0100

    implemented ShutdownCommand

--- discordchess/src/main/java/me/dzikimlecz/discordchess/event/commands/ShutdownCommand.java
+++ discordchess/src/main/java/me/dzikimlecz/discordchess/event/commands/ShutdownCommand.java
@@ -1,13 +1,25 @@
 package me.dzikimlecz.discordchess.event.commands;
 
+import me.duncte123.botcommons.BotCommons;
+import me.dzikimlecz.discordchess.config.IConfig;
+import me.dzikimlecz.discordchess.config.ILogs;
 import me.dzikimlecz.discordchess.util.CommandContext;
+import me.dzikimlecz.discordchess.util.CommandHelpData;
 
 import java.util.List;
 
 public class ShutdownCommand extends AbstractCommand {
+
+	public ShutdownCommand(IConfig<String> config, ILogs logs) {
+		super(config, logs);
+		help.setUsage("Command permitted only for bot developers. Shuts it down on all servers.");
+	}
+
 	@Override
 	public void handle(CommandContext context) {
-
+		var author = context.getAuthor();
+		if (author.getId().equals(config.get("owner id"))) BotCommons.shutdown();
+		else context.getChannel().sendMessage("You don't have permission to do this").queue();
 	}
 
 	@Override
@@ -18,10 +30,5 @@ public class ShutdownCommand extends AbstractCommand {
 	@Override
 	public List<String> getAliases() {
 		return List.of("off");
-	}
-
-	@Override
-	public List<String> getHelp() {
-		return null;
 	}
 }
