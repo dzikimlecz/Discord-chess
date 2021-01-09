0a05a7e15f39978a59eb3d7c3cb26f1d0db101bb
dzikimlecz <j.pul312@gmail.com>
2021-01-09 16:56:55 +0100

    implemented command help api

--- discordchess/src/main/java/me/dzikimlecz/discordchess/event/commands/AbstractCommand.java
+++ discordchess/src/main/java/me/dzikimlecz/discordchess/event/commands/AbstractCommand.java
@@ -1,16 +1,31 @@
 package me.dzikimlecz.discordchess.event.commands;
 
+import me.dzikimlecz.discordchess.config.IConfig;
+import me.dzikimlecz.discordchess.config.ILogs;
 import me.dzikimlecz.discordchess.util.CommandContext;
+import me.dzikimlecz.discordchess.util.CommandHelpData;
 
 import java.util.List;
 
 public abstract class AbstractCommand {
-	
+
+	protected final IConfig<String> config;
+	protected final ILogs logs;
+	protected final CommandHelpData help;
+
+	public AbstractCommand(IConfig<String> config, ILogs logs) {
+		this.config = config;
+		this.logs = logs;
+		help = new CommandHelpData(getName(), getAliases(), "");
+	}
+
 	public abstract void handle(CommandContext context);
 	
 	public abstract String getName();
 	
-	public abstract List<String> getHelp();
+	public CommandHelpData getHelp() {
+		return help;
+	}
 	
 	public List<String> getAliases() {
 		return List.of();
