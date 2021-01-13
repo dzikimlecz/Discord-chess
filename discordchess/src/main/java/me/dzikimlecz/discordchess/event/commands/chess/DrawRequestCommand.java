aa48777779f2af1208e101fb1cd02e83e21cd6e9
dzikimlecz <j.pul312@gmail.com>
2021-01-13 15:10:45 +0100

    commands for handling draws

---
+++ discordchess/src/main/java/me/dzikimlecz/discordchess/event/commands/chess/DrawRequestCommand.java
@@ -0,0 +1,29 @@
+package me.dzikimlecz.discordchess.event.commands.chess;
+
+import me.dzikimlecz.chessapi.game.board.Color;
+import me.dzikimlecz.discordchess.config.IConfig;
+import me.dzikimlecz.discordchess.config.ILogs;
+import me.dzikimlecz.discordchess.game.ChessGameManager;
+import me.dzikimlecz.discordchess.util.CommandContext;
+
+import java.util.List;
+
+public class DrawRequestCommand extends ChessCommand {
+	public DrawRequestCommand(IConfig<String> config, ILogs logs, ChessGameManager manager) {
+		super("drawreq", List.of("reqdraw", "rd", "dr"), config, logs, manager);
+	}
+
+	@Override
+	public void handle(CommandContext context) {
+		var channel = context.getChannel();
+		var author = context.getAuthor();
+		var info = gamesManager.getInfo(channel);
+		for (Color color : Color.values()) {
+			if (info.getPlayer(color).equals(author)) {
+				gamesManager.requestDraw(channel, color);
+				return;
+			}
+		}
+		channel.sendMessage("You aren't in any game on this channel!").queue();
+	}
+}
---
+++ discordchess/src/main/java/me/dzikimlecz/discordchess/event/commands/chess/DrawResponseCommand.java
@@ -0,0 +1,54 @@
+package me.dzikimlecz.discordchess.event.commands.chess;
+
+import me.dzikimlecz.chessapi.game.board.Color;
+import me.dzikimlecz.discordchess.config.IConfig;
+import me.dzikimlecz.discordchess.config.ILogs;
+import me.dzikimlecz.discordchess.game.ChessGameManager;
+import me.dzikimlecz.discordchess.util.CommandContext;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class DrawResponseCommand extends ChessCommand {
+	public DrawResponseCommand(IConfig<String> config, ILogs logs, ChessGameManager manager) {
+		super("draw", List.of("drs, rsd"), config, logs, manager);
+
+	}
+
+	@Override
+	public void handle(CommandContext context) {
+		var args = context.getArgs();
+		var channel = context.getChannel();
+		var author = context.getAuthor();
+		var listener = gamesManager.getListener(channel);
+		var requester = listener.drawRequester();
+		if (args.size() < 1) {
+			sendUsage(channel);
+			return;
+		}
+
+		var checkRequestStatus = checkRequest(channel, author, requester);
+		if (checkRequestStatus != null) {
+			channel.sendMessage(checkRequestStatus).queue();
+			return;
+		}
+
+		var response = args.get(0);
+		switch (response) {
+			case "accept", "a", "ok", "yes", "si" -> listener.replyToDraw(true);
+			case "deny", "d", "nah", "nope", "no" -> listener.replyToDraw(false);
+			default -> channel.sendMessage("Usage: " + help.usage()).queue();
+		}
+	}
+
+	@Nullable
+	private String checkRequest(TextChannel channel, User author, Color requester) {
+		if (requester == null)
+			return "There are no pending draw requests on this channel";
+		var properResponder = requester.opposite();
+		return (gamesManager.getInfo(channel).getPlayer(properResponder).equals(author)) ?
+				null : "You are not allowed to respond to the pending request on this channel";
+	}
+}
