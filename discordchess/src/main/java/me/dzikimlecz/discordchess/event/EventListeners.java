075c07e87151b910401b45bfc089e4bf76c10f99
dzikimlecz <j.pul312@gmail.com>
2021-01-13 15:08:31 +0100

    added commands to manager

--- discordchess/src/main/java/me/dzikimlecz/discordchess/event/EventListeners.java
+++ discordchess/src/main/java/me/dzikimlecz/discordchess/event/EventListeners.java
@@ -2,8 +2,12 @@ package me.dzikimlecz.discordchess.event;
 
 import me.dzikimlecz.discordchess.config.IConfig;
 import me.dzikimlecz.discordchess.config.ILogs;
+import me.dzikimlecz.discordchess.event.commands.HelpCommand;
+import me.dzikimlecz.discordchess.event.commands.ShutdownCommand;
+import me.dzikimlecz.discordchess.event.commands.chess.*;
 import me.dzikimlecz.discordchess.event.listeners.MessagesListener;
 import me.dzikimlecz.discordchess.event.listeners.OnReadyListener;
+import me.dzikimlecz.discordchess.game.ChessGameManager;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -13,14 +17,31 @@ public class EventListeners extends ListenerAdapter {
 
 	private final OnReadyListener onReadyListener;
 	private final MessagesListener messagesListener;
-	private final CommandManager manager;
-	
+	private final CommandManager commandManager;
+	private final ChessGameManager gameManager;
+
 	public EventListeners(IConfig<String> config, ILogs logs) {
-		manager = new CommandManager(config, logs);
-		onReadyListener = new OnReadyListener(config, logs, manager);
-		messagesListener = new MessagesListener(config, logs, manager);
+		commandManager = new CommandManager(config, logs);
+		gameManager = new ChessGameManager(config, logs);
+		addCommands(config, logs);
+		onReadyListener = new OnReadyListener(config, logs, commandManager);
+		messagesListener = new MessagesListener(config, logs, commandManager);
 	}
-	
+
+	private void addCommands(IConfig<String> config, ILogs logs) {
+		commandManager.addCommands(
+				new ShutdownCommand(config, logs),
+				new HelpCommand(config, logs, commandManager)
+		);
+		commandManager.addCommands(
+				new GameInitCommand(config, logs, gameManager),
+				new MoveCommand(config, logs, gameManager),
+				new GameForceStopCommand(config, logs, gameManager),
+				new DrawRequestCommand(config, logs, gameManager),
+				new DrawResponseCommand(config, logs, gameManager)
+		);
+	}
+
 	@Override
 	public void onReady(@NotNull ReadyEvent event) {
 		onReadyListener.onReady(event);
