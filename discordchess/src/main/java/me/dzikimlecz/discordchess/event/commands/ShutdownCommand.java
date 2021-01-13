3c41b7a2254b2ce311272ea990085ad3f6ee35ea
dzikimlecz <j.pul312@gmail.com>
2021-01-13 15:06:10 +0100

    added informing about shutdown in logs

--- discordchess/src/main/java/me/dzikimlecz/discordchess/event/commands/ShutdownCommand.java
+++ discordchess/src/main/java/me/dzikimlecz/discordchess/event/commands/ShutdownCommand.java
@@ -11,18 +11,22 @@ import java.util.List;
 public class ShutdownCommand extends AbstractCommand {
 
 	public ShutdownCommand(IConfig<String> config, ILogs logs) {
-		super(config, logs);
-		name = "shutdown";
-		aliases = List.of("off");
+		super("shutdown", List.of("off"), config, logs);
 		help.setCmdInfo("Command permitted only for bot developers. Shuts it down on all servers.");
-		help.setUsage(MessageFormat.format("{0}{1}", config.get("prefix"), name));
+		help.setUsage(MessageFormat.format("{0}{1}", config.get("prefix"), name()));
 	}
 
 	@Override
 	public void handle(CommandContext context) {
 		var author = context.getAuthor();
-		if (author.getId().equals(config.get("owner id"))) BotCommons.shutdown();
-		else context.getChannel().sendMessage("You don't have permission to do this").queue();
+		if (!author.getId().equals(config.get("owner id"))) {
+			context.getChannel().sendMessage("You don't have permission to do this").queue();
+			return;
+		}
+		logs.write("Shutting down.", ShutdownCommand.class);
+		var jda = context.getJDA();
+		jda.shutdown();
+		BotCommons.shutdown(jda);
 	}
 
 }
