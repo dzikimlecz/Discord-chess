01c10b1e4dc16909f8e38337d805313166a5ec91
dzikimlecz <j.pul312@gmail.com>
2021-01-13 21:31:11 +0100

    optimised board rendering process

--- src/main/java/me/dzikimlecz/discordchess/util/ChessImageProcessor.java
+++ src/main/java/me/dzikimlecz/discordchess/util/ChessImageProcessor.java
@@ -1,6 +1,5 @@
 package me.dzikimlecz.discordchess.util;
 
-import me.dzikimlecz.chessapi.game.board.Board;
 import me.dzikimlecz.chessapi.game.board.Color;
 import me.dzikimlecz.chessapi.game.board.pieces.ChessPiece;
 import org.jetbrains.annotations.NotNull;
@@ -9,31 +8,29 @@ import org.jetbrains.annotations.Nullable;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 import java.text.MessageFormat;
 
 public class ChessImageProcessor {
 
 	private static final int SQUARE_SIDE_LENGTH = 512;
 	private static final int BOARD_SIDE_LENGTH = 8 * SQUARE_SIDE_LENGTH;
-	private static final BufferedImage WHITE_TILE;
-	private static final BufferedImage BLACK_TILE;
+	public static final BufferedImage BOARD;
 
 	
 	static {
-		WHITE_TILE = new BufferedImage(SQUARE_SIDE_LENGTH,
-		                                      SQUARE_SIDE_LENGTH,
-		                                      BufferedImage.TYPE_4BYTE_ABGR);
-		for (int y = 0; y < SQUARE_SIDE_LENGTH; y++)
-			for (int x = 0; x < SQUARE_SIDE_LENGTH; x++) 
-				WHITE_TILE.setRGB(x, y, new java.awt.Color(0xFEFEFE).getRGB());
+		BOARD = new BufferedImage(BOARD_SIDE_LENGTH,
+		                          BOARD_SIDE_LENGTH,
+		                          BufferedImage.TYPE_4BYTE_ABGR);
+		for (int y = 0; y < BOARD_SIDE_LENGTH; y++) {
+			for (int x = 0; x < BOARD_SIDE_LENGTH; x++) {
+				boolean isSquareBlack =
+						((x / SQUARE_SIDE_LENGTH) % 2) == ((y / SQUARE_SIDE_LENGTH) % 2);
+				var color = (isSquareBlack) ?
+						new java.awt.Color(0x263238) : new java.awt.Color(0xFEFEFE);
+				BOARD.setRGB(x, y, color.getRGB());
+			}
+		}
 
-		BLACK_TILE = new BufferedImage(SQUARE_SIDE_LENGTH,
-		                                  SQUARE_SIDE_LENGTH,
-		                                  BufferedImage.TYPE_4BYTE_ABGR);
-		for (int y = 0; y < SQUARE_SIDE_LENGTH; y++)
-			for (int x = 0; x < SQUARE_SIDE_LENGTH; x++)
-				BLACK_TILE.setRGB(x, y, new java.awt.Color(0x263238).getRGB());
 	}
 
 	public BufferedImage generateImageOfBoard(ChessPiece[][] pieces) {
@@ -42,47 +39,24 @@ public class ChessImageProcessor {
 		var boardImage = new BufferedImage(BOARD_SIDE_LENGTH,
 		                                   BOARD_SIDE_LENGTH,
 		                                   BufferedImage.TYPE_4BYTE_ABGR);
-		for (int y = 0, piecesLength = pieces.length; y < piecesLength; y++) {
-			ChessPiece[] chessPieces = pieces[y];
-			for (int x = 0, chessPiecesLength = chessPieces.length; x < chessPiecesLength; x++) {
-				ChessPiece piece = chessPieces[x];
-				var color = ((y % 2) == (x % 2)) ? Color.BLACK : Color.WHITE;
-				var squareImage = generateSquareImage(piece, color);
-				int squareStartX = x * SQUARE_SIDE_LENGTH;
-				int squareStartY = y * SQUARE_SIDE_LENGTH;
-				for (int copyingY = 0; copyingY < SQUARE_SIDE_LENGTH; copyingY++) {
-					for (int copyingX = 0; copyingX < SQUARE_SIDE_LENGTH; copyingX++) {
-						var rgb = squareImage.getRGB(copyingX, copyingY);
-						boardImage.setRGB(
-								squareStartX + copyingX,
-								squareStartY + copyingY,
-								rgb
-						);
+		for (int row = 0; row < 8; row++) {
+			for (int line = 0; line < 8; line++) {
+				var piece = pieces[row][line];
+				boolean noPiece = (piece == null);
+				var pieceImage = (noPiece) ? null : getPieceImage(piece);
+				for (int rawY = 0; rawY < SQUARE_SIDE_LENGTH; rawY++) {
+					int y = row * SQUARE_SIDE_LENGTH + rawY;
+					for (int rawX = 0; rawX < SQUARE_SIDE_LENGTH; rawX++) {
+						int x = line * SQUARE_SIDE_LENGTH + rawX;
+						int pieceImageRGB = (noPiece) ? 0 : pieceImage.getRGB(rawX, rawY);
+						double pieceImageAlpha = pieceImageRGB / 1E6;
+						int rgb = (pieceImageAlpha == 0) ? BOARD.getRGB(x, y) : pieceImageRGB;
+						boardImage.setRGB(x, y, rgb);
 					}
 				}
-
 			}
 		}
 		return boardImage;
-	}
-
-	private BufferedImage generateSquareImage(@Nullable ChessPiece piece, Color squareColor) {
-		var squareBackground = (squareColor == Color.WHITE) ? WHITE_TILE : BLACK_TILE;
-		if (piece == null) return squareBackground;
-		var pieceImage = getPieceImage(piece);
-
-		var bufferedImage = new BufferedImage(SQUARE_SIDE_LENGTH,
-		                                      SQUARE_SIDE_LENGTH,
-		                                      BufferedImage.TYPE_4BYTE_ABGR);
-		for (int y = 0; y < SQUARE_SIDE_LENGTH; y++) {
-			for (int x = 0; x < SQUARE_SIDE_LENGTH; x++) {
-				int rgb = pieceImage.getRGB(x, y);
-				var alpha = rgb / 10E6;
-				int rgbToInsert = (alpha == 0) ? squareBackground.getRGB(x, y) : rgb;
-				bufferedImage.setRGB(x, y, rgbToInsert);
-			}
-		}
-		return bufferedImage;
 	}
 
 	private BufferedImage getPieceImage(@NotNull ChessPiece piece) {
