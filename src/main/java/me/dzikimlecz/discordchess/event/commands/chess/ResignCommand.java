68c41affd56ae568928a053a09076677f265e962
dzikimlecz <j.pul312@gmail.com>
2021-01-19 21:11:06 +0100

    added command for resigning from a game

--- src/main/java/me/dzikimlecz/discordchess/event/commands/chess/ResignCommand.java
+++ src/main/java/me/dzikimlecz/discordchess/event/commands/chess/ResignCommand.java
@@ -1,5 +1,6 @@
 package me.dzikimlecz.discordchess.event.commands.chess;
 
+import me.dzikimlecz.chessapi.game.board.Color;
 import me.dzikimlecz.discordchess.config.IConfig;
 import me.dzikimlecz.discordchess.config.ILogs;
 import me.dzikimlecz.discordchess.game.ChessGameManager;
@@ -14,6 +15,23 @@ public class ResignCommand extends ChessCommand {
 
 	@Override
 	public void handle(CommandContext context) {
-
+		var channel = context.getChannel();
+		var author = context.getAuthor();
+		var info = gamesManager.getInfo(channel);
+		var userOptional =
+				List.of(info.getPlayer(Color.WHITE), info.getPlayer(Color.BLACK)).stream().filter(
+						author::equals).findFirst();
+		if (userOptional.isEmpty()) {
+			channel.sendMessage("You aren't in any game on this channel").queue();
+			return;
+		}
+		gamesManager.forceClose(channel);
+		var loser = userOptional.get();
+		channel.sendMessage(loser.getAsMention() + " resigned!").queue();
+		var winnerColor = info.getPlayer(Color.WHITE).equals(loser) ? Color.BLACK :Color.WHITE;
+		info.setWinner(winnerColor);
+		var winner = info.getWinner();
+		assert winner != null;
+		channel.sendMessage("Winner: " + winner.getAsMention()).queue();
 	}
 }
