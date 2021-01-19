3b5c1b8aa36d6286a0cda335c331d33deb7a3d12
dzikimlecz <j.pul312@gmail.com>
2021-01-19 21:37:40 +0100

    added class for sending an image to a channel

---
+++ src/main/java/me/dzikimlecz/discordchess/event/commands/ImageSender.java
@@ -0,0 +1,42 @@
+package me.dzikimlecz.discordchess.event.commands;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.TextChannel;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ImageSender {
+
+	public void sendImage(BufferedImage image,
+	                      TextChannel channel,
+	                      String title) throws IOException {
+		String fileName =
+				MessageFormat.format("{0}{1}.png",
+				                     LocalDateTime.now().format(DateTimeFormatter.ISO_TIME)
+						                     .replaceAll("[:+]", "_"),
+				                     ThreadLocalRandom.current().nextInt(100));
+		var temp = new File("temp", fileName);
+		var embed = new EmbedBuilder();
+		embed.setTitle(title);
+		ImageIO.write(image, "png", temp);
+		var file = new FileInputStream(temp);
+		embed.setImage("attachment://board.png");
+		channel.sendFile(file, "board.png").embed(embed.build()).queue();
+		new Timer().schedule(new TimerTask() {
+			@Override
+			public void run() {
+				temp.delete();
+			}
+		}, 500);
+	}
+}
