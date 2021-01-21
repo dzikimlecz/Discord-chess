2bb456e384810c89e0448240b582ff16f97ffac8
dzikimlecz <j.pul312@gmail.com>
2021-01-21 12:04:22 +0100

    enabled required Cache flags and Intents

--- src/main/java/me/dzikimlecz/discordchess/Bot.java
+++ src/main/java/me/dzikimlecz/discordchess/Bot.java
@@ -6,6 +6,8 @@ import me.dzikimlecz.discordchess.config.Logs;
 import me.dzikimlecz.discordchess.event.EventListeners;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 public class Bot {
 	public static void main(String[] args) throws Exception {
@@ -14,6 +16,12 @@ public class Bot {
 
 		JDABuilder.createDefault(config.get("token"))
 				.addEventListeners(new EventListeners(config, logs))
+				.enableIntents(GatewayIntent.GUILD_MEMBERS,
+				               GatewayIntent.GUILD_PRESENCES)
+				.enableCache(CacheFlag.ACTIVITY,
+				             CacheFlag.CLIENT_STATUS,
+				             CacheFlag.MEMBER_OVERRIDES,
+				             CacheFlag.ROLE_TAGS)
 				.setActivity(Activity.playing("chess"))
 				.build();
 	}
