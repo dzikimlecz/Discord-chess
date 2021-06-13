13fdd4d65f483a18eb6fa14da445bcc5379372e4
dzikimlecz <j.pul312@gmail.com>
2021-06-13 18:10:10 +0200

    cleaned up code

--- src/main/java/me/dzikimlecz/discordchess/Bot.java
+++ src/main/java/me/dzikimlecz/discordchess/Bot.java
@@ -6,23 +6,26 @@ import me.dzikimlecz.discordchess.config.Logs;
 import me.dzikimlecz.discordchess.event.EventListeners;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+import static net.dv8tion.jda.api.requests.GatewayIntent.GUILD_MEMBERS;
+import static net.dv8tion.jda.api.requests.GatewayIntent.GUILD_PRESENCES;
+import static net.dv8tion.jda.api.utils.cache.CacheFlag.*;
 
 public class Bot {
-	public static void main(String[] args) throws Exception {
-		var config = new Config();
-		var logs = new Logs();
+    public static void main(String[] args) throws Exception {
+        var config = new Config();
 
 		JDABuilder.createDefault(config.get("token"))
-				.addEventListeners(new EventListeners(config, logs))
-				.enableIntents(GatewayIntent.GUILD_MEMBERS,
-				               GatewayIntent.GUILD_PRESENCES)
-				.enableCache(CacheFlag.ACTIVITY,
-				             CacheFlag.CLIENT_STATUS,
-				             CacheFlag.MEMBER_OVERRIDES,
-				             CacheFlag.ROLE_TAGS)
-				.setActivity(Activity.playing("chess"))
-				.build();
+				.addEventListeners(new EventListeners(config, new Logs()))
+				.enableIntents(
+				        GUILD_MEMBERS,
+                        GUILD_PRESENCES
+                ).enableCache(
+				        ACTIVITY,
+                        CLIENT_STATUS,
+                        MEMBER_OVERRIDES,
+                        ROLE_TAGS
+                ).setActivity(Activity.playing("chess"))
+                .build();
 	}
 }
