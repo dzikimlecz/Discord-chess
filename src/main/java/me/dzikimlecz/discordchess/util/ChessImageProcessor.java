e06fd502122272cde4ce24eddd8d7985ecb7d670
dzikimlecz <j.pul312@gmail.com>
2021-09-16 18:36:19 +0200

    removed cached backgrounds from ram to files

--- src/main/java/me/dzikimlecz/discordchess/util/ChessImageProcessor.java
+++ src/main/java/me/dzikimlecz/discordchess/util/ChessImageProcessor.java
@@ -6,6 +6,8 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.text.MessageFormat;
 import java.util.List;
@@ -13,65 +15,69 @@ import java.util.Objects;
 
 public class ChessImageProcessor {
 
-    public static final BufferedImage WHITE_SIDE_BOARD;
-    public static final BufferedImage BLACK_SIDE_BOARD;
+    private static final File WHITE_SIDE_BOARD_FILE = new File("temp/whiteSideBoard.png");
+    private static final File BLACK_SIDE_BOARD_FILE = new File("temp/blackSideBoard.png");
     private static final int SQUARE_SIDE_LENGTH = 512;
     private static final int BOARD_SIDE_LENGTH = 8 * SQUARE_SIDE_LENGTH;
+    final static int DARK_SQUARE_COLOR_RGB = 0x263238;
+    final static int LIGHT_SQUARE_COLOR_RGB = 0xFEFEFE;
 
     static {
-        WHITE_SIDE_BOARD = new BufferedImage(BOARD_SIDE_LENGTH,
-                BOARD_SIDE_LENGTH,
-                BufferedImage.TYPE_4BYTE_ABGR);
-        BLACK_SIDE_BOARD = new BufferedImage(BOARD_SIDE_LENGTH,
-                BOARD_SIDE_LENGTH,
-                BufferedImage.TYPE_4BYTE_ABGR);
-        for (int y = 0; y < BOARD_SIDE_LENGTH; y++) {
-            for (int x = 0; x < BOARD_SIDE_LENGTH; x++) {
-                boolean isSquareBlackFromWhiteSide =
-                        ((x / SQUARE_SIDE_LENGTH) % 2) == ((y / SQUARE_SIDE_LENGTH) % 2);
-                final int rgbForWhite;
-                final int rgbForBlack;
-                if (isSquareBlackFromWhiteSide) {
-                    rgbForWhite = 0x263238;
-                    rgbForBlack = 0xFEFEFE;
-                } else {
-                    rgbForWhite = 0xFEFEFE;
-                    rgbForBlack = 0x263238;
-                }
-                var colorForWhite = new java.awt.Color(rgbForWhite);
-                var colorForBlack = new java.awt.Color(rgbForBlack);
-                WHITE_SIDE_BOARD.setRGB(x, y, colorForWhite.getRGB());
-                BLACK_SIDE_BOARD.setRGB(x, y, colorForBlack.getRGB());
-            }
-        }
-
+        if (!WHITE_SIDE_BOARD_FILE.exists() || !BLACK_SIDE_BOARD_FILE.exists())
+            initBackgroundFiles();
     }
 
-    public BufferedImage generateImageOfBoard(List<List<ChessPiece>> pieces) {
-        if (pieces == null || pieces.size() != 8 || pieces.get(0).size() != 8)
-            throw new IllegalArgumentException("Illegal board format");
-        var boardImage = new BufferedImage(BOARD_SIDE_LENGTH,
+    private static void initBackgroundFiles() {
+        createFiles();
+        var WHITE_SIDE_BOARD = new BufferedImage(BOARD_SIDE_LENGTH,
                 BOARD_SIDE_LENGTH,
                 BufferedImage.TYPE_4BYTE_ABGR);
-        boardImage.setData(WHITE_SIDE_BOARD.getRaster());
+        var BLACK_SIDE_BOARD = new BufferedImage(BOARD_SIDE_LENGTH,
+                BOARD_SIDE_LENGTH,
+                BufferedImage.TYPE_4BYTE_ABGR);
+        fillBackgrounds(WHITE_SIDE_BOARD, BLACK_SIDE_BOARD);
+        saveBackgrounds(WHITE_SIDE_BOARD, BLACK_SIDE_BOARD);
+    }
 
-        for (int row = 0; row < 8; row++) {
-            for (int line = 0; line < 8; line++) {
-                var piece = pieces.get(row).get(line);
-                if (piece == null) continue;
-                var pieceImage = getPieceImage(piece);
-                for (int rawY = 0; rawY < SQUARE_SIDE_LENGTH; rawY++) {
-                    int y = row * SQUARE_SIDE_LENGTH + rawY;
-                    for (int rawX = 0; rawX < SQUARE_SIDE_LENGTH; rawX++) {
-                        int x = line * SQUARE_SIDE_LENGTH + rawX;
-                        int pieceImageRGB = pieceImage.getRGB(rawX, rawY);
-                        double pieceImageAlpha = pieceImageRGB / 1E6;
-                        if (pieceImageAlpha != 0) boardImage.setRGB(x, y, pieceImageRGB);
-                    }
-                }
+    private static void createFiles() {
+        WHITE_SIDE_BOARD_FILE.mkdirs();
+        BLACK_SIDE_BOARD_FILE.mkdirs();
+        try {
+            WHITE_SIDE_BOARD_FILE.createNewFile();
+            BLACK_SIDE_BOARD_FILE.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException("Could not initialize", e);
+        }
+    }
+
+    private static void fillBackgrounds(BufferedImage WHITE_SIDE_BOARD, BufferedImage BLACK_SIDE_BOARD) {
+        for (int y = 0; y < BOARD_SIDE_LENGTH; y++) {
+            for (int x = 0; x < BOARD_SIDE_LENGTH; x++) {
+                WHITE_SIDE_BOARD.setRGB(x, y, getColorForWhite(x, y).getRGB());
+                BLACK_SIDE_BOARD.setRGB(x, y, getColorForBlack(x, y).getRGB());
             }
         }
-        return boardImage;
+    }
+
+    private static java.awt.Color getColorForWhite(int x, int y) {
+        boolean isSquareBlackFromWhiteSide = ((x / SQUARE_SIDE_LENGTH) % 2) == ((y / SQUARE_SIDE_LENGTH) % 2);
+        final int rgbForWhite = isSquareBlackFromWhiteSide ? DARK_SQUARE_COLOR_RGB : LIGHT_SQUARE_COLOR_RGB;
+        return new java.awt.Color(rgbForWhite);
+    }
+
+    private static java.awt.Color getColorForBlack(int x, int y) {
+        boolean isSquareBlackFromWhiteSide = ((x / SQUARE_SIDE_LENGTH) % 2) == ((y / SQUARE_SIDE_LENGTH) % 2);
+        final int rgbForBlack = isSquareBlackFromWhiteSide ? LIGHT_SQUARE_COLOR_RGB : DARK_SQUARE_COLOR_RGB;
+        return new java.awt.Color(rgbForBlack);
+    }
+
+    private static void saveBackgrounds(BufferedImage WHITE_SIDE_BOARD, BufferedImage BLACK_SIDE_BOARD) {
+        try {
+            ImageIO.write(WHITE_SIDE_BOARD, "png", WHITE_SIDE_BOARD_FILE);
+            ImageIO.write(BLACK_SIDE_BOARD, "png", BLACK_SIDE_BOARD_FILE);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not initialize", e);
+        }
     }
 
     public BufferedImage generateImageOfBoard(List<List<ChessPiece>> pieces,
@@ -81,7 +87,7 @@ public class ChessImageProcessor {
         var boardImage = new BufferedImage(BOARD_SIDE_LENGTH,
                 BOARD_SIDE_LENGTH,
                 BufferedImage.TYPE_4BYTE_ABGR);
-        boardImage.setData(WHITE_SIDE_BOARD.getRaster());
+        boardImage.setData(getBoard(colorOnTheBottom).getRaster());
 
         int firstGeneratedRow = (colorOnTheBottom == Color.BLACK) ? 0 : 7;
         int limit = (colorOnTheBottom == Color.BLACK) ? 8 : -1;
@@ -107,9 +113,35 @@ public class ChessImageProcessor {
         return boardImage;
     }
 
+    private BufferedImage getBoard(Color colorOnTheBottom) {
+        try {
+            return readBoard(colorOnTheBottom);
+        } catch (IOException e) {
+            initBackgroundFiles();
+            try {
+                return readBoard(colorOnTheBottom);
+            } catch (IOException ex) {
+                throw new RuntimeException("Could not read board", e);
+            }
+        }
+    }
+
+    private BufferedImage readBoard(Color colorOnTheBottom) throws IOException {
+        return ImageIO.read(colorOnTheBottom == Color.WHITE ? WHITE_SIDE_BOARD_FILE: BLACK_SIDE_BOARD_FILE);
+    }
+
     @NotNull
     private BufferedImage getPieceImage(@NotNull ChessPiece piece) {
-        final String pathNotFilled = "pieces/pngs/{0}/{1}.png";
+        try {
+            return ImageIO.read(getFromResources(getPath(piece)));
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Piece without matching image", e);
+        }
+    }
+
+    @NotNull
+    private String getPath(@NotNull ChessPiece piece) {
+        final String pathTemplate = "pieces/pngs/{0}/{1}.png";
         var color = piece.color().name().toLowerCase();
         var name = switch (piece.toString()) {
             case "P" -> "pawn";
@@ -120,12 +152,7 @@ public class ChessImageProcessor {
             case "K" -> "king";
             default -> throw new IllegalStateException("Unexpected value: " + piece);
         };
-        String path = MessageFormat.format(pathNotFilled, color, name);
-        try {
-            return ImageIO.read(getFromResources(path));
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Piece without matching image", e);
-        }
+        return MessageFormat.format(pathTemplate, color, name);
     }
 
     @NotNull
